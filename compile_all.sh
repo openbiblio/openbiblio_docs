f0b63189922a3ea965df4e3456e2e3799db45a1a
Jane Sandberg <sandbej@linnbenton.edu>
2016-03-26 13:12:19 -0700

    adding info about special subfields

--- compile_all.sh
+++ compile_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 a2x root.txt
-asciidoc root.txt
+asciidoc -a toc root.txt
 for file in *; do
-test -d "$file" && a2x "$file"/root.txt && asciidoc "$file"/root.txt
+test -d "$file" && a2x "$file"/root.txt && asciidoc -a toc "$file"/root.txt
 done
