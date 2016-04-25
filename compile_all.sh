5f414b1880ba2d14dbdd07ff4252caf5ba276b26
Jane Sandberg <sandbej@linnbenton.edu>
2016-04-25 06:56:12 -0700

    removing revision history page from pdf output

--- compile_all.sh
+++ compile_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mysql-autodoc obib -Hlocalhost -uobib -p
-a2x root.txt
+a2x --dblatex-opts "-P latex.output.revhistory=0" root.txt
 asciidoc -a toc -a icons root.txt
 for file in *; do
-test -d "$file" && a2x "$file"/root.txt && asciidoc -a toc "$file"/root.txt
+test -d "$file" && a2x --dblatex-opts "-P latex.output.revhistory=0" "$file"/root.txt && asciidoc -a toc "$file"/root.txt
 done
