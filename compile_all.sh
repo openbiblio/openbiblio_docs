76f05bb3b9d41e07525a80a10ceca9fc3501ceef
Jane Sandberg <sandbej@linnbenton.edu>
2017-04-11 18:13:09 -0700

    compiling

--- compile_all.sh
+++ compile_all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #mysql-autodoc obib -Hlocalhost -uobib -p -fschema.html
-a2x --dblatex-opts "-P latex.output.revhistory=0" root.txt
+#a2x --dblatex-opts "-P latex.output.revhistory=0" root.txt
+a2x --fop root.txt
 asciidoc -a toc -a icons root.txt
 for file in *; do
-test -d "$file" && a2x --dblatex-opts "-P latex.output.revhistory=0" "$file"/root.txt && asciidoc -a toc "$file"/root.txt
+test -d "$file" && a2x --fop "$file"/root.txt && asciidoc -a toc "$file"/root.txt
 done
